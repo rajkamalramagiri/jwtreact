e58f5e5d31d867dfdfef0492a130e0e9ec41109d
rajkamal <rajkamal.smit@gmail.com>
2023-12-15 18:01:38 +0530

    REFRESH TOKEN ADDED

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import axios from 'axios';
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [token, setToken] = useState(localStorage.getItem('accessToken') || null);
   const [userData, setUserData] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const getUser = () => {
+     // Fetch user data using the token
+     axios.get('http://localhost:5000/user', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        setUserData(response.data.user);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error.response.status);
+        if (error.response.status === 403) {
+          axios.post('http://localhost:5000/token', { refreshToken: localStorage.getItem('refreshToken') || null })
+            .then(response => {
+              const { accessToken } = response.data;
+              console.log('responseaccess',accessToken)
+              localStorage.setItem('accessToken', accessToken);
+        setToken(accessToken);
+
+              // getUser();
+          })
+        }
+      });
+  }
 
   useEffect(() => {
     if (token) {
-      // Fetch user data using the token
-      axios.get('http://localhost:5000/user', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then(response => {
-          console.log('usrdat',response)
-          setUserData(response.data.user);
-        })
-        .catch(error => {
-          console.error('Error fetching user data:', error);
-        });
+      getUser();
     }
   }, [token]);
 
@@ -31,10 +44,12 @@ const App = () => {
     // Simulate a login request, replace with your actual authentication logic
     axios.post('http://localhost:5000/login', { username, password })
       .then(response => {
-        console.log('res',response)
-        const { token } = response.data;
-        setToken(token);
-        localStorage.setItem('token', token);
+        const { accessToken, refreshToken } = response.data;
+        setToken(accessToken);
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+
+        
       })
       .catch(error => {
         console.error('Login failed:', error);
@@ -74,6 +89,7 @@ const App = () => {
             <div>
               <h2>Welcome, {userData ? userData.username : 'User'}!</h2>
               <button onClick={handleLogout}>Logout</button>
+              <button onClick={getUser}>Get User</button>
             </div>
           ) : (
             <Redirect to="/login" />
